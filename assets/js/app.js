94575c7c8a863b80ef4e8786b2ce68b025c342c5
Diego Armando Hernandez Villalba <diegodhv1@gmail.com>
2019-08-16 11:39:54 -0500

    formatting

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,22 @@
 $(function () {
-    /**
-     * Validate login form
-     * 
-     */
-    $('#btn-access').on('click', function () {
+  /**
+   * Validate login form
+   * 
+   */
+  $('#btn-access').on('click', function () {
+    $('#alert-success').removeClass("show");
+    $('#alert-error').removeClass("show");
+    if ($("#form-login")[0].checkValidity()) {
+      $('#alert-success').addClass("show");
+      setTimeout(function () {
         $('#alert-success').removeClass("show");
+      }, 2000)
+    } else {
+      $('#alert-error').addClass("show");
+      setTimeout(function () {
         $('#alert-error').removeClass("show");
-        if ($("#form-login")[0].checkValidity()) {
-            $('#alert-success').addClass("show");
-            setTimeout(function () {
-                $('#alert-success').removeClass("show");
-            }, 2000)
-        } else {
-            $('#alert-error').addClass("show");
-            setTimeout(function () {
-                $('#alert-error').removeClass("show");
-            }, 2000)
-        }
-    });
+      }, 2000)
+    }
+  });
 });
 
