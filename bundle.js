3dbfed97d36f78415dbf86aa0fc6ec06ab4aab42
Diego Armando Hernandez Villalba <diegodhv1@gmail.com>
2019-08-16 14:47:22 -0500

    test

--- bundle.js
+++ bundle.js
@@ -4,10 +4,17 @@ $(function () {
    * Validate login form
    * 
    */
+  function validEmail(email) {
+    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+  };
+
   $('#btn-access').on('click', function () {
     $('#alert-success').removeClass("show");
     $('#alert-error').removeClass("show");
-    if ($("#form-login")[0].checkValidity()) {
+    var email = $('.email-input').val();
+    console.log(validEmail(email));
+    if ($("#form-login")[0].checkValidity() && validEmail(email) ) {
       $('#alert-success').addClass("show");
       setTimeout(function () {
         $('#alert-success').removeClass("show");
