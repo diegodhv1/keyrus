2e533d0ffc49444564f427bb36c5111a63ae7fb1
Diego Armando Hernandez Villalba <diegodhv1@gmail.com>
2019-08-16 10:49:58 -0500

    fixing js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,23 @@
-var $ = require("jquery");
-require('bootstrap');
-
-$(document).ready(function(){
+$(document).ready(function () {
 
     /**
      * Validate login form
      * 
      */
-    $('#btn-access').on('click', function() {
+    $('#btn-access').on('click', function () {
         $('#alert-success').removeClass("show");
         $('#alert-error').removeClass("show");
         if ($("#form-login")[0].checkValidity()) {
             $('#alert-success').addClass("show");
-            setTimeout(function(){
+            setTimeout(function () {
                 $('#alert-success').removeClass("show");
             }, 4000)
         } else {
             $('#alert-error').addClass("show");
-            setTimeout(function(){
+            setTimeout(function () {
                 $('#alert-error').removeClass("show");
             }, 4000)
-        }
+        }   
     });
-}); 
+});
 
